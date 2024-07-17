962197a41761f6fb50fda8bd0028d67380f38ff4
Paula Cari <paulola0223@gmail.com>
2024-07-17 14:50:36 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,13 @@
+  var typed = new Typed('.meuTextoPersonalizado', {
+    strings: [
+        'Maria Paula.', 
+        'Full stack.', 
+         'Python.', 
+         'Desenho.'
+],
+    typeSpeed: 100,
+    backSpeed: 60,
+    cursorChar:'...',
+    loop:true,
+    loopCount: 3,
+  });
